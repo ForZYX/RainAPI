aa90ed79b29df918ad9c734d671f69e63ff02e19
zyx <2604280445@qq.com>
2024-11-17 23:47:47 +0800

    submodule gateway

---
+++ rainapi-backend/rainapi-gateway/src/test/java/com/rainapi/rainapigateway/RainapiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainapi.rainapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainapiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
